613faac613c641fa31a286cbc8f2291a5f19c4a3
Daf <afahd.ali@gmail.com>
2022-10-06 02:01:38 +0200

    added user model

---
+++ src/DafDev.FootballManagerNot.GroupManagement.Web/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace DafDev.FootballManagerNot.GroupManagement.Web.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? Lastname { get; set; }
+}
