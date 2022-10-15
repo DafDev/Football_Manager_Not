f1871eb3e7c3a2c40f6609c34ea66a3e05fab72a
Daf <afahd.ali@gmail.com>
2022-10-15 21:50:45 +0200

    implemented dependency injection

---
+++ src/DafDev.FootballManagerNot.Contracts/Services/IGroupService.cs
@@ -0,0 +1,17 @@
+﻿using DafDev.FootballManagerNot.GroupManagement.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DafDev.FootballManagerNot.Contracts.Services;
+
+public interface IGroupService
+{
+    IReadOnlyCollection<Group> GetAll();
+    Group GetById(int id);
+    Group GetByName(string name);
+    Group Update(Group group);
+    Group Add(Group group);
+}
