45cc1486ba95a4743aeda601cfb6290525306ac5
Daf <afahd.ali@gmail.com>
2022-10-19 23:33:10 +0200

    added configuration

---
+++ src/DafDev.FootballManagerNot.GroupManagement.Web/Configuration/SomeRootConfiguration.cs
@@ -0,0 +1,7 @@
+namespace DafDev.FootballManagerNot.GroupManagement.Web.Configuration;
+
+public class SomeRootConfiguration
+{
+    public const string SomeRoot = "SomeRoot";
+    public SomeSubRootConfiguration SomSubRoot { get; set; }
+}
